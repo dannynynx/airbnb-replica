76141a2b6fb2b8c58064cc49e44e9c70b8705a6b
Daniel <ledanielhuynh@gmail.com>
2023-11-07 04:41:34 +1100

    completed 2.1.1, 2.1.2, 2.1.3

--- frontend/src/helpers/helpers.js
+++ frontend/src/helpers/helpers.js
@@ -1,135 +1,28 @@
-import { BACKEND_PORT } from '../config.json';
+import config from '../config.json';
+import { useNavigate } from 'react-router-dom';
 
-const postRequest = (path, body, token) => {
-  fetch(`http://localhost:${BACKEND_PORT}` + path, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(body)
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        alert(data.error);
-      } else {
-        return data;
-      }
-    });
-};
+const BACKEND_PORT = config.BACKEND_PORT;
+const navigate = useNavigate();
 
-const getRequest = (path, token) => {
-  fetch(`http://localhost:${BACKEND_PORT}` + path, {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        alert(data.error);
-      } else {
-        return data;
-      }
-    });
-};
-
-const putRequest = (path, body, token) => {
-  return fetch(`http://localhost:${BACKEND_PORT}` + path, {
-    method: 'PUT',
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(body)
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        alert(data.error);
-      } else {
-        return data;
-      }
-    });
-};
-
-const deleteRequest = (path, token) => {
-  return fetch(`http://localhost:${BACKEND_PORT}` + path, {
-    method: 'DELETE',
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        alert(data.error);
-      } else {
-        return data;
-      }
-    });
-};
-
-// User Auth
-// Managing user authentication and authorisation
-
-export const postRegister = (body, token) => {
-  return postRequest('/user/auth/register', body, token)
-};
-
-export const postLogin = (body, token) => {
-  return postRequest('/user/auth/login', body, token)
-};
-
-export const postLogout = (body, token) => {
-  return postRequest('/user/auth/logout', body, token)
-};
-
-// Listing Management
-// Managing listings
-
-export const getListings = (token) => {
-  return getRequest('/listings', token)
-};
-
-export const postNewListing = (body, token) => {
-  return postRequest('/listings/new', body, token)
-};
-
-export const getListingFromId = (listingId, token) => {
-  const path = '/listings/' + listingId
-  return getRequest(path, token)
-};
-
-export const putUpdateListing = (listingId, body, token) => {
-  const path = '/listings/' + listingId
-  return putRequest(path, body, token)
+export function isLoggedIn () {
+  return !!localStorage.getItem('token');
 }
 
-export const deleteListing = (listingId, token) => {
-  const path = '/listings/' + listingId
-  return deleteRequest(path, token)
+export const logout = async () => {
+  try {
+    const response = await fetch(`http://localhost:${BACKEND_PORT}/user/auth/logout`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 200) {
+      localStorage.removeItem('token');
+      navigate('/login');
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-export const putPublishListing = (listingId, body, token) => {
-  const path = '/listings/publish/' + listingId
-  return putRequest(path, body, token)
-}
-
-export const putUnpublishListing = (listingId, token) => {
-  const path = '/listings/unpublish/' + listingId
-  const body = {}
-  return putRequest(path, body, token)
-}
-
-export const putListingReview = (listingId, bookingId, body, token) => {
-  const path = '/listings/' + listingId + '/review/' + bookingId
-  return putRequest(path, body, token)
-}
-
-// Booking Management
-// Managing bookings
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,8 +8,17 @@ module.exports = {
       fontFamily: {
         nunito: ['Nunito Sans', 'sans'],
       },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-in-out',
+      },
     },
-  },
-  variants: {},
-  plugins: [],
-}
+    variants: {},
+    plugins: [],
+  }
+};
